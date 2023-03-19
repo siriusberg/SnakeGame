4a8739cbde223414f622f627c8718d558ed116ac
Ocpcgo <abualaanadwi1@gmail.com>
2023-03-19 19:28:39 +0700

    Update Snake.py
    Window name

--- Snake.py
+++ Snake.py
@@ -15,7 +15,7 @@ dis_width = 800
 dis_height = 600
  
 dis = pygame.display.set_mode((dis_width, dis_height))
-pygame.display.set_caption('Snake Game')
+pygame.display.set_caption('Sneaky Serpent')
  
 clock = pygame.time.Clock()
  
